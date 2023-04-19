13cb6600fba81eda1c52ea50e55c58019299b902
Sitkevich88 <Sitkevich2002@mail.ru>
2023-04-19 13:49:15 +0300

    Сделал увольнение по ресту

--- src/main/java/ru/ntv/controllers/boss/BossJournalistController.java
+++ src/main/java/ru/ntv/controllers/boss/BossJournalistController.java
@@ -6,13 +6,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.ntv.dto.response.boss.JournalistListResponse;
 import ru.ntv.dto.response.boss.JournalistResponse;
-import ru.ntv.dto.response.common.ArticleResponse;
-import ru.ntv.entity.articles.Article;
 import ru.ntv.exception.NotRightRoleException;
 import ru.ntv.service.UserService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @Validated
@@ -36,10 +33,9 @@ public class BossJournalistController {
         return ResponseEntity.ok(new JournalistListResponse(journalists));
     }
 
-    @PostMapping("retire")
-    ResponseEntity<?> retireJournalist(@RequestParam int idJournalist) throws NotRightRoleException {
+    @DeleteMapping
+    ResponseEntity<?> dismissJournalist(@RequestParam int journalistId) throws NotRightRoleException {
 
-        return ResponseEntity.ok(userService.retireJournalist(idJournalist));
+        return ResponseEntity.ok(userService.dismissJournalist(journalistId));
     }
-    
 }
--- src/main/java/ru/ntv/service/UserService.java
+++ src/main/java/ru/ntv/service/UserService.java
@@ -1,8 +1,6 @@
 package ru.ntv.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.ntv.dto.response.boss.JournalistResponse;
@@ -15,7 +13,6 @@ import ru.ntv.repo.user.RoleRepository;
 import ru.ntv.repo.user.UserRepository;
 
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -34,10 +31,10 @@ public class UserService {
 
 
     @Transactional(transactionManager = "transactionManager")
-    public String retireJournalist(int idJournalist) throws NotRightRoleException{
+    public String dismissJournalist(int journalistId) throws NotRightRoleException{
 
-        final var journalist = userRepository.findById(idJournalist).orElseThrow(); //todo throw custom Exception if user is not found
-        System.out.println(journalist.getLogin() + " " + journalist.getId() + " " + journalist.getRole().getRoleName());
+        final var journalist = userRepository.findById(journalistId).orElseThrow(); //todo throw custom Exception if user is not found
+        //System.out.println(journalist.getLogin() + " " + journalist.getId() + " " + journalist.getRole().getRoleName());
         if (!Objects.equals(journalist.getRole().getRoleName(), DatabaseRole.ROLE_JOURNALIST.name())) throw new NotRightRoleException("Это не журналист"); //todo throw custom Exception that isn't journalist
 
         journalist.setRole(
@@ -49,21 +46,19 @@ public class UserService {
 
         List<Article> articles = articleRepository.findAllByJournalistName(journalist.getLogin());
 
-        articles.forEach(e -> System.out.println(e.getJournalistName()));
+        //articles.forEach(e -> System.out.println(e.getJournalistName()));
         articles.forEach( a -> a.setJournalistName(null));
 
 
         userRepository.save(journalist);
         articleRepository.saveAll(articles);
 
-        return "уволен";
-
-
+        return "Dismissed";
     }
 
 
     public JournalistResponse getJournalistById(int id) {
-        final var user = userRepository.findById(id).get(); //todo throw custom Exception if user is not found
+        final var user = userRepository.findById(id).orElseThrow(); //todo throw custom Exception if user is not found
         
         return convertUserToJournalist(user);
     }
