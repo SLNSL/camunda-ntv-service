b97188ed19bebeb1af25f35a306e7bf4fec624b6
Sitkevich88 <Sitkevich2002@mail.ru>
2023-05-13 13:56:01 +0300

    Разбил проект на модули

--- main_service/src/main/java/ru/ntv/NTVApplication.java
+++ main_service/src/main/java/ru/ntv/NTVApplication.java
@@ -2,9 +2,7 @@ package ru.ntv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
---
+++ tg_service/src/main/java/ru/ntv/tg_service/TgServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.ntv.tg_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgServiceApplication.class, args);
+    }
+
+}
