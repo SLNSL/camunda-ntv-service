a2f6e2561e2fc1b528a9680141cf9fc9e4258698
Sitkevich88 <Sitkevich2002@mail.ru>
2023-05-30 23:34:50 +0300

    Убрал лишнее

--- mail_service/src/main/java/ru/ntv/mail_service/repo/EmailUserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.ntv.mail_service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.ntv.mail_service.entity.EmailUser;
-
-public interface EmailUserRepository extends JpaRepository<EmailUser, Integer> {
-    EmailUser findByUserId(Integer userId);
-}
--- mail_service/src/main/java/ru/ntv/mail_service/repo/EmailUserThemeRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.ntv.mail_service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.ntv.mail_service.entity.EmailUserTheme;
-
-public interface EmailUserThemeRepository extends JpaRepository<EmailUserTheme, Integer> {
-}
